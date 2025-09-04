182ff6bdffebc3136e08f76ac3991c03f6f73c89
yayfalafels <yayfalafels@gmail.com>
2025-09-04 12:07:03 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# demo-sentiment-analysis
-Sentiment analysis in Python on complaint cases
+# Sentiment Analysis
+Sentiment analysis in Python for complaint cases data
+
+## Complaint cases
+
+- data format
+- fields available
+    - unstructured free-text
+    - reporter attributes
+    - company attributes
+
+## 01 Sentiment
+estimate sentiment from unstructured data from complaint cases
+
+## 02 Forecasting
+forecast sentiment trends using ARIMA and time series analysis
+
+## 03 Regression
+Predict sentiment from other attributes in the data set
+
+
---
+++ data_import.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""import and clean-up case data
+"""
+# dependencies ---------------------------------------------
+import pandas as pd
+
+
+# constants --------------------------------------------------
+RAW_EXCEL_FILE = 'Complaintcasesall.xls'
+CLEAN_DATA_FILE = 'case_data_clean.csv'
+FIRST_YEAR = 2020
+YEARS = [FIRST_YEAR + i for i in range(5)]
+
+
+# module variables --------------------------------------------
+case_data_df = None
+
+
+# data import ------------------------------------------------
+def data_cleanup():
+    # 01 read from excel
+    year_dfs = []
+    column_ref = ''
+    for year in YEARS:
+        df = pd.read_excel(RAW_EXCEL_FILE, sheet_name=str(year))
+        # year_columns = ','.join(list(df.columns))
+
+        if year == FIRST_YEAR:
+            column_ref = df.columns # year_columns
+        else:
+            assert column_ref == df.columns, f'column mismatch ref for year {year}'
+
+        df['year'] = year
+        year_dfs.append(df)
+        print(f'found {len(df)} rows for year {year}')
+        print(f'columns: {df.columns}')
+
+    # 02 consolidate single table by year
+    case_data_df = pd.concat(year_dfs)
+
+    # 03 export to CSV
+    case_data_df.to_csv(CLEAN_DATA_FILE, index=False)
+
+    print(f'saved consolidated case data by year to {CLEAN_DATA_FILE}.')
+
+# entry point -------------------------------------------------
+def run():
+    print('case complaint sentiment analysis:')
+    data_cleanup()
+
+
+if __name__ == '__main__':
+    run()
